059844827e4ccd4554ebf3f0150bef7ea5c3c823
jeremy_large <jeremylarge@yahoo.com>
2021-03-25 17:33:45 +0000

    Two purposes: 1) locate classes in their wards; and 2) implement less lockdown in deprived areas (this is a linear effect in income decile).

--- lib/codit/population/networks/city.py
+++ lib/codit/population/networks/city.py
@@ -12,8 +12,8 @@ from codit.population.networks.home_locations import Home, get_home_samples
 EPHEMERAL_CONTACT = 0.1  # people per day
 WITHIN_BUILDING_CONTACT = 0.75
 
-class CityPopulation(FixedNetworkPopulation):
 
+class CityPopulation(FixedNetworkPopulation):
     def __init__(self, n_people, society, person_type=None, lockdown_config=None):
         Population.__init__(self, n_people, society, person_type=person_type)
         self.households, self.workplaces, self.classrooms, self.care_homes, self.buildings = build_city_structures(self.people)
@@ -39,22 +39,61 @@ class CityPopulation(FixedNetworkPopulation):
 
         return static_cliques + dynamic_cliques + building_cliques
 
-    def build_city_cliques(self, lockdown_config):
+    def build_city_cliques(self, lockdown_config, by_deprivation=True):
 
-        def _suppress(grouping, name, factor):
-            n_retained = int(len(grouping) * (1 - factor))
-            logging.info(f"{factor * 100}% of {name} closed by lockdown, leaving {n_retained} open.")
-            return random.sample(grouping, n_retained)
+        workplaces = _suppress(self.workplaces, 'workplaces', lockdown_config['workplaces'],
+                               by_deprivation=by_deprivation)
 
-        workplaces = _suppress(self.workplaces, 'workplaces', lockdown_config['workplaces'])
-        classrooms = _suppress(self.classrooms, 'classrooms', lockdown_config['classrooms'])
+        classrooms = _suppress(self.classrooms, 'classrooms', lockdown_config['classrooms'],
+                               by_deprivation=by_deprivation)
 
         return self.households + workplaces + classrooms + self.care_homes
 
 
-def build_city_structures(people):
+def _suppress(workplaces, name, lockdown_factor, by_deprivation=True):
+
+    def income_decile(p):
+        """
+        :param p: person
+        :return: a float from most deprived to least in [1., 2., 3., 4., 5., 6., 7., 8., 9., 10.]
+        """
+        return p.home.lsoa.features['Income_Decile']
+
+    def _dep(people):
+        # :return: the mean decile of the people, transformed linearly to lie between -1 and 1.
+        if not by_deprivation:
+            return 0.
+        return (np.mean([income_decile(person) for person in people]) / 10. - 0.55) / 0.45
+
+    def _prob_lockdown(grp):
+        """
+        :param grp: a group of people
+        :return: probability that this group will be locked down. While taking account of the global lockdown factor,
+        this is linear in grp's mean_income_decile, and is set so that if all members of grp are of decile 10,
+        then the probability of lockdown is exactly one.
+        """
+        return lockdown_factor + (_dep(grp) * (1 - lockdown_factor))
+
+    open_workplaces = [g for g in workplaces if random.random() > _prob_lockdown(g)]
+    report_lockdown(income_decile, lockdown_factor, name, open_workplaces)
+    return open_workplaces
+
+
+def report_lockdown(income_decile, lockdown_factor, name, open_workplaces):
+    """
+    Just do some logging
+    """
+    workplace_deciles = [income_decile(p) for g in open_workplaces for p in g]
+    logging.info(f"{lockdown_factor * 100}% of {name} closed by lockdown, "
+                 f"leaving {len(open_workplaces)} open, "
+                 f"of average Income Decile "
+                 f"{np.mean(workplace_deciles):2.2f} (and st dev {np.std(workplace_deciles):2.2f}).")
+
+
+def build_city_structures(people, schools_by_ward=True):
     """
     :param people: a list of population.covid.PersonCovid() objects
+    :param schools_by_ward: bool. If True, then school classrooms will contain only children of the same ward
     :return: a list of little sets, each is a 'clique' in the graph, some are households, some are workplaces
     each individual should belong to exactly one household and one workplace
     for example: [{person_0, person_1, person_2}, {person_0, person_10, person_54, person_88, person_550, person_270}]
@@ -66,7 +105,7 @@ def build_city_structures(people):
     buildings = build_buildings(people)
     report_size(buildings, 'buildings')
 
-    classrooms = build_class_groups(people)
+    classrooms = build_classes_by_ward(people) if schools_by_ward else build_class_groups(people)
 
     working_age_people = [p for p in people if MINIMUM_WORKING_AGE < p.age < MAXIMUM_WORKING_AGE]
     teachers = random.sample(working_age_people, len(classrooms))
@@ -104,11 +143,22 @@ def report_size(care_homes, ch):
 
 def build_class_groups(people, class_size=30):
     classrooms = []
-    for kids_age in range(MINIMUM_CLASS_AGE, MAXIMUM_CLASS_AGE+1):
+    for kids_age in range(MINIMUM_CLASS_AGE, MAXIMUM_CLASS_AGE + 1):
         schoolkids = [p for p in people if p.age == kids_age]
         random.shuffle(schoolkids)
         classrooms += build_workplaces(schoolkids, force_size=class_size)
     return classrooms
+
+
+def build_classes_by_ward(people, class_size=30):
+
+    def _inhabitants(wrd):
+        return [p for p in people if p.home.ward == wrd]
+
+    classes = []
+    for ward in {p.home.ward for p in people}:
+        classes += build_class_groups(_inhabitants(ward), class_size)
+    return classes
 
 
 def build_buildings(people):
