d0cf1ce0cfe43aaa3cf5277b5d0534d58bacb1ff
justin-zhida-jia <forestbill@msn.com>
2021-04-13 12:31:03 +0100

    Add comments to Lambeth and Wandsworth's population and Openstreetmap query str

--- lib/codit/population/networks/city_config/city_cfg.py
+++ lib/codit/population/networks/city_config/city_cfg.py
@@ -30,16 +30,18 @@ city_paras = \
         },
         'Lambeth':
         {
-            # Total population estimate of Leeds (2019) Source: ONS* https://observatory.leeds.gov.uk/population/
+            # Total population estimate of Lambeth (mid-2018)
+            # Source: London Councils* https://directory.londoncouncils.gov.uk/demographics/lambeth/
             'population': 325917,
-            # Area str of Leeds for request to OpenStreetMap
+            # Area str of Lambeth for request to OpenStreetMap
             'area_str': "area['ISO3166-2'='GB-LBH'][admin_level=8]"
         },
         'Wandsworth':
         {
-            # Total population estimate of Leeds (2019) Source: ONS* https://observatory.leeds.gov.uk/population/
+            # Total population estimate of Wandsworth (mid-2019) Source: Wandsworth Gov*
+            # https://www.wandsworth.gov.uk/planning-and-building-control/planning-policy/local-plan/local-plan-monitoring/local-plan-population/population-estimates-and-projections/
             'population': 329700,
-            # Area str of Leeds for request to OpenStreetMap
+            # Area str of Wandsworth for request to OpenStreetMap
             'area_str': "area['ISO3166-2'='	GB-WND'][admin_level=8]"
         }
     }
