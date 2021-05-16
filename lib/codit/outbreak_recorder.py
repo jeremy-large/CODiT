d35ccd7da9260710263685bd4a2441380f9f05f2
jeremy_large <jeremylarge@yahoo.com>
2021-05-16 18:15:52 +0100

    mapping completed

--- lib/codit/outbreak_recorder.py
+++ lib/codit/outbreak_recorder.py
@@ -105,6 +105,7 @@ class WardComponent:
         self.wards = list({p.home.ward for p in o.pop.people if p.home.ward.name})
         self.infected = []
         self.infectious = []
+        self.indian_variant = []
         self.positive_tests = []
         self.expected_death = []
         self.expected_hospitalization = []
@@ -128,6 +129,12 @@ class WardComponent:
     def update(self, o):
         self.infected.append([o.time] +
                              [sum([p.infected for p in self.people_of[w]]) / len(self.people_of[w]) for w in self.wards]
+                             )
+
+        self.indian_variant.append([o.time] +
+                             [len([p for p in self.people_of[w]
+                                   if p.disease and p.disease.name == 'B.1.617.2'])
+                              for w in self.wards]
                              )
 
         self.infectious.append([o.time] +
@@ -181,17 +188,19 @@ class WardComponent:
         df = df.iloc[np.flip(order)].T
         return df
 
-    def map_incidence(self, title='', end_date=False):
-        infect_over_time = self.dataframe(self.infectious)
+    def map_incidence(self, wardlevel_data, title='', end_date=False, per_hundred_k=False):
+        infect_over_time = self.dataframe(wardlevel_data)
         incidence = infect_over_time.mean().T
         if end_date:
             incidence = infect_over_time.loc[infect_over_time.index[-1]]
         incidence = pd.DataFrame(incidence)
         incidence.columns = ['incidence']
-        results = gpd.GeoDataFrame(pd.merge(incidence * 100000, self.shapes.geometry, right_index=True, left_index=True))
+        if per_hundred_k:
+            incidence *= 100000
+        results = gpd.GeoDataFrame(pd.merge(incidence, self.shapes.geometry, right_index=True, left_index=True))
         fig, ax = plt.subplots(1, 1)
         ax.axes.get_yaxis().set_visible(False)
         ax.axes.get_xaxis().set_visible(False)
-        plt.title(title + '\ninfection rates across wards')
-        _ = results.plot(column='incidence', cmap='Purples', legend=True, ax=ax,
-                         legend_kwds={'label': "infectious individuals per 100,000"})
+        results.plot(column='incidence', cmap='Purples', legend=True, ax=ax, edgecolor='black',
+                         legend_kwds={'label': "infectious individuals"})
+        _ = plt.title(title + '\ninfection rates across wards' + (" per 100,000" if per_hundred_k else ""))
